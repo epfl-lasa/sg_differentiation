ffe3705876e4c59c96c31906a4c5afb4b4d7bae6
nad <nbfigueroa@gmail.com>
2016-12-19 15:41:17 +0100

    Rewrote multi-dim code

--- test/multidim_test.cpp
+++ test/multidim_test.cpp
@@ -7,78 +7,80 @@
 
 using namespace SGF;
 
-template<typename T>
-void load_matrix(const char *fname, T &target) {
-
-  std::ifstream myfile(fname);
-  std::string line;
-  float f;
-  int n_rows=0;
-  std::vector<float> v;
-  while(getline(myfile,line)){
-    ++n_rows;
-    std::istringstream line_stream(line);
-    while(!line_stream.eof()){
-      line_stream>>f;
-      v.push_back(f);
-    }
-  }
-  int n_cols = v.size()/n_rows;
-  target.resize(n_rows,n_cols);
-  for(int i=0; i<n_rows;i++){
-    for(int j=0;j<n_cols;j++){
-      target(i,j) = v[i*n_cols + j];
-    }
-  }
-}
-
 
 int main(int argc, char *argv[]){
     int order = 2;
-    int winlen = 21;
+    int winlen = 1;
     float sample_time = 1e-3;
     int dim = 200;
-    SGF::Vec inp(dim), inp1(1), inp2(1);
+    int iters = 50;
+    SGF::Vec inp(dim);
 
-    load_matrix("./data/test_data_1_inp.txt", inp1);
-    load_matrix("./data/test_data_2_inp.txt", inp2);
-
-    printf ("****200-d Test****\n");
-
-    for(int i=0; i<inp1.size(); i++){
-            inp << inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),
-                    inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i), inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i),inp1(i), inp2(i);
-    }
 
     SGF::SavitzkyGolayFilter filter(dim, order, winlen, sample_time);
-
-    clock_t t;
-    t = clock();
+    for (int j = 0 ; j < iters ; j++ )
     {
 
-        clock_t t_add;
-        t_add = clock();
-        filter.AddData(inp);
-        t_add = clock() - t_add;
-        printf ("filter.AddData: %f seconds.\n",((float)t_add)/CLOCKS_PER_SEC);
+      printf ("****%d-d (Step %d) Test****\n",dim, j);  
+      for(int i=0; i<dim; i++){
+            inp(i) = (float) rand();  
+      }
 
-        clock_t t_get;
-        t_get = clock();
-        filter.GetOutput(0, inp);
-        t_get = clock() - t_get;
-        printf ("filter.GetOutput 0: %f seconds.\n",((float)t_get)/CLOCKS_PER_SEC);
+      int retcode;
+      clock_t t;
+      t = clock();
+      {
+
+          clock_t t_add;
+          t_add = clock();
+          retcode = filter.AddData(inp);
+          t_add = clock() - t_add;
+          if (retcode == 0){
+            printf ("filter.AddData: %f seconds.\n",((float)t_add)/CLOCKS_PER_SEC);  
+          }
+          else if (retcode == -1){
+           printf ("filter not initialized correctly :(.\n");   
+          }
+          
+
+          clock_t t_get;
+          t_get = clock();
+          retcode = filter.GetOutput(0, inp);
+          t_get = clock() - t_get;
+          if (retcode == 0){
+            printf ("filter.GetOutput 0: %f seconds.\n",((float)t_get)/CLOCKS_PER_SEC);
+          }
+          else if (retcode == -1){
+           printf ("filter not initialized correctly :(.\n");   
+          }
+
+          clock_t t_get1;
+          t_get1 = clock();
+          retcode = filter.GetOutput(1, inp);
+          t_get1 = clock() - t_get1;
+          if (retcode == 0){
+            printf ("filter.GetOutput 1: %f seconds.\n",((float)t_get1)/CLOCKS_PER_SEC);
+          }        
+          else if (retcode == -1){
+           printf ("filter not initialized correctly :(.\n");   
+          }
+
+          clock_t t_get2;
+          t_get2 = clock();
+          retcode = filter.GetOutput(2, inp);
+          t_get2 = clock() - t_get2;
+          if (retcode == 0){
+            printf ("filter.GetOutput 2: %f seconds.\n",((float)t_get2)/CLOCKS_PER_SEC);
+          }        
+          else if (retcode == -1){
+           printf ("filter not initialized correctly :(.\n");   
+          }
+      }
+      t = clock() - t;
+      printf ("Total: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+
+  }
 
 
-        clock_t t_get1;
-        t_get1 = clock();
-        filter.GetOutput(1, inp);
-        t_get1 = clock() - t_get1;
-        printf ("filter.GetOutput 1: %f seconds.\n",((float)t_get1)/CLOCKS_PER_SEC);
-
-    }
-    t = clock() - t;
-    printf ("Total: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
-
-}
-
+}    
 
